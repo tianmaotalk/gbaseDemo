7973d1f50cae9f036059258f9ed1709d48966c1d
tianmaotalk <tianmaotalk@163.com>
2019-05-18 19:55:43 +0800

    init

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	@Autowired
+	private DataSource dataSource;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+	}
+
+	@RequestMapping("/test")
+	public String test() {
+		Connection conn = null;
+		try {
+			conn = this.dataSource.getConnection();
+			System.out.println("adfadsf+++++++++" + conn.getSchema());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (conn == null) {
+				System.err.println("=============");
+			} else {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+		return "ok";
+	}
+
+}
